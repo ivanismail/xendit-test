4e6d440f77b74bf966fedb31aefe727aa6d5aef6
Ivan Ismail <ivanismail2@gmail.com>
2022-01-12 16:36:06 +0700

    add: list virtual account

--- app/Http/Controllers/API/Payment/XenditController.php
+++ app/Http/Controllers/API/Payment/XenditController.php
@@ -2,10 +2,21 @@
 
 namespace App\Http\Controllers\API\Payment;
 
-use App\Http\Controllers\Controller;
+use Xendit\Xendit;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class XenditController extends Controller
 {
-    //
+    private $token = 'xnd_public_production_8byWorUUdVxnctDDZW4k5ScGo1Q42KFyBJ35Al6f9ZSqnhi7bsF4TbEGwCT';
+
+    public function getListVA()
+    {
+        Xendit::setApiKey($this->token);
+        $getVABanks = \Xendit\VirtualAccounts::getVABanks();
+
+        return response()->json([
+            'data' => $getVABanks
+        ])->setStatusCode(200);
+    }
 }
