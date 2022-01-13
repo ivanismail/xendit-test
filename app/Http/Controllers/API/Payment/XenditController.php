0d439e9753f7b97a0cde82f919a56d52e222a1cc
Ivan Ismail <ivanismail2@gmail.com>
2022-01-13 14:26:10 +0700

    add: create virtual acount

--- app/Http/Controllers/API/Payment/XenditController.php
+++ app/Http/Controllers/API/Payment/XenditController.php
@@ -8,7 +8,8 @@ use App\Http\Controllers\Controller;
 
 class XenditController extends Controller
 {
-    private $token = 'xnd_public_production_8byWorUUdVxnctDDZW4k5ScGo1Q42KFyBJ35Al6f9ZSqnhi7bsF4TbEGwCT';
+    private $token = 'xnd_development_Hsu9PXfGESzi3Gr94Fe1vXSgrExffn65XG9aS4Wrh66hEc9bSk2HzXFyzmpl';
+    // private $token = 'xnd_development_Fvb4acvM32jIQMa8K1FtyC6wXfwY7wsJLWAfuhNjsSLNiJpy5EdDQyCB40n9i9';
 
     public function getListVA()
     {
@@ -17,6 +18,25 @@ class XenditController extends Controller
 
         return response()->json([
             'data' => $getVABanks
+        ])->setStatusCode(200);
+    }
+
+    public function createVA(Request $request)
+    {
+        Xendit::setApiKey($this->token);
+        $params = [ 
+            "external_id" => \uniqid(),
+            "bank_code" => $request->bank,
+            "name" => $request->name,
+            "expected_amount" => $request->amount,
+            "is_closed" => true
+
+          ];
+        
+        $createVA = \Xendit\VirtualAccounts::create($params);
+
+        return response()->json([
+            'data' => $createVA
         ])->setStatusCode(200);
     }
 }
